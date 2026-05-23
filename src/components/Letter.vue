632e05316f0e36d71d6035cea2d1dce0ceec3226
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-05-23 19:53:02 -0400

    letter

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="letter">
-    <p>Happy Birthday Johanna!! 🎂</p>
-    <p>I made this website to wish you a very happy birthday and to show you thanks for being the best best friend there is. (And also because I am too broke to give you a proper gift, lmao TT). I hope your birthday was amazing despite you being busy with school. What gifts did you receive? Hopefully, they were cooler than this, haha.</p>
-    <p>We met a decade ago now, right? We've grown a lot since then. We've spent so much of this friendship far apart but I never feel the distance. I'm glad we are such close friends. Must have to do with our neurospiciness. We've been friends for so long and I wish our friendship lasts forever.</p>
-    <p>I want to thank you for always being there for me. I know it can be annoying to put up with me but you have done a good job of it. Despite the distance, your support helps a lot and means a lot to me. Hopefully, I have been able to support you a little bit too. </p>
-    <p>Hmm, if this is not gift enough, maybe I can give you something in the campaign. Anyway, enough sappiness. TLDR. Happy Birthday!!</p>
+    <p>Happy Birthday Salma!! 🎂</p>
+    <p>This was a bit of a rush job but I thought I would just deploy it anyway. You might read this during dinner, at karaoke or maybe the day after. I don't know lol.</p>
+    <p>But I hope you enjoy the night!!</p>
   </div>
 </template>
 
